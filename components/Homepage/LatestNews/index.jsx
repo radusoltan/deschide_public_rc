022f4f733d0a6a1dd4ed04a456abea65ee98f213
Radu Soltan <radusoltan@gmail.com>
2024-08-12 02:17:46 +0200

    initial commit 16

--- components/Homepage/LatestNews/index.jsx
+++ components/Homepage/LatestNews/index.jsx
@@ -25,7 +25,7 @@ export const LastArticles = () => {
     <div className="w-full bg-white sticky top-1">
 
       {/*<div className="flex flex-row flex-wrap ">*/}
-      {lastArticles?.articles.slice(0, 5).map(article => !article.is_video && <ArticleItem key={article._id}
+      {lastArticles?.articles.slice(0, 5).map(article => !article._source.is_video && <ArticleItem key={article._id}
                                                                       article={article._source}/>)}
       <div
           className="flex-shrink max-w-full w-full md:w-1/2 lg:w-full xl:w-full px-3 pb-3 pt-3 sm:pt-0 border-b-2 sm:border-b-0 border-dotted border-gray-100">
@@ -38,7 +38,7 @@ export const LastArticles = () => {
           </div>
         </a>
       </div>
-      {lastArticles?.articles.slice(5).map(article => !article.is_video && <ArticleItem key={article._id}
+      {lastArticles?.articles.slice(5).map(article => !article._source.is_video && <ArticleItem key={article._id}
                                                                    article={article._source}/>)}
 
         <Link href={`/${locale}/articles/all?page=1&limit=15`} ><h1 className="text-center max-w-full w-full font-light text-gray-500 p-5 hover:bg-gray-300 hover:text-gray-500 mx-auto mb-6">{
